7ea83c4b694fae8a2911c640614234c61315f594
PamelaCleveland <pamelac021@gmail.com>
2022-07-26 01:03:06 -0400

    Implement Auth Middleware to Populate Me Query

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
