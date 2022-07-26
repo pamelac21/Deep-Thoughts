78a12f4469b72525042fec9780bb5cc9fd972526
PamelaCleveland <pamelac021@gmail.com>
2022-07-26 00:07:35 -0400

    Create Add User and Login Mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Thought } = require("../models");
 
 const resolvers = {
@@ -24,6 +25,27 @@ const resolvers = {
       return Thought.findOne({ _id });
     },
   },
+
+  Mutation: {
+    addUser: async () => {
+
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+      
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        const correctPw = await user.isCorrectPassword(password);
+      
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+      
+        return user;
+      }
+  }
 };
 
 module.exports = resolvers;
