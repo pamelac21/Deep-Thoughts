86c575019554a5b55575b88339d5f5e05796dbeb
PamelaCleveland <pamelac021@gmail.com>
2022-07-29 00:35:04 -0400

    Implement the Add Thought Mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,6 +24,21 @@ export const ADD_USER = gql`
   }
 `;
 
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
 export const ADD_FRIEND = gql`
   mutation addFriend($id: ID!) {
     addFriend(friendId: $id) {
